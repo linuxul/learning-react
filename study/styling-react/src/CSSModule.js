e49eba712fdfdd54a65f164be6cc384b381f6400
linuxul <jsseo@eflash.co.kr>
2024-02-17 18:03:54 +0900

    [feat] 09. 컴포넌트 스타일링 CSSModule scss #9.6

--- study/styling-react/src/CSSModule.js
+++ study/styling-react/src/CSSModule.js
@@ -1,12 +1,34 @@
-import styles from './CSSModule.module.css';
+import classNames from 'classnames/bind';
+// import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
+
+const cx = classNames.bind(styles);
 
 const CSSModele = () => {
-  console.log(styles)
+  console.log(styles);
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx('wrapper', 'inverted')}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
-  )
-}
+  );
+};
 
-export default CSSModele
+// const CSSModele = () => {
+//   console.log(styles)
+//   return (
+//     <div className={[styles.wrapper, styles.inverted].join(' ')}>
+//       안녕하세요, 저는 <span className="something">CSS Module!</span>
+//     </div>
+//   )
+// }
+
+// const CSSModele = () => {
+//   console.log(styles)
+//   return (
+//     <div className={`${styles.wrapper} ${styles.inverted}`}>
+//       안녕하세요, 저는 <span className="something">CSS Module!</span>
+//     </div>
+//   )
+// }
+
+export default CSSModele;
