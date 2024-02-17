39372e6a7a8f53c0a38a0ac7db8046f3bbbd71f3
linuxul <jsseo@eflash.co.kr>
2024-02-17 11:22:47 +0900

    [feat] 06. Component LifeCycle #7

--- study/hello-react/src/App.js
+++ study/hello-react/src/App.js
@@ -8,15 +8,43 @@ import EventPracticeFunc from './EventPracticeFunc';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
     return (
-      <IterationSample></IterationSample>
-    )
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <IterationSample></IterationSample>
+//     )
+//   }
+// }
 
 // class App extends Component {
 //   render() {
