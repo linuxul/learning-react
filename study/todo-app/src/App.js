622aadf4955e5d8597feaf17507d8121c1912c90
linuxul <jsseo@eflash.co.kr>
2024-02-17 22:24:13 +0900

    [feat] 10.일정 관리 웹 애플리케이션 - 배열 객체 추가 #10.3

--- study/todo-app/src/App.js
+++ study/todo-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -24,9 +24,24 @@ const App = () => {
     }
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert></TodoInsert>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
       <TodoList todos={todos}></TodoList>
     </TodoTemplate>
   );
