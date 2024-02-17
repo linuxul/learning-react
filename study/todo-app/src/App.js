0ac9c0e564ddafef3a1eaa115ced5061ed9cbb1f
linuxul <jsseo@eflash.co.kr>
2024-02-17 22:11:00 +0900

    [feat] 10.일정 관리 웹 애플리케이션 만들기 #10.2

--- study/todo-app/src/App.js
+++ study/todo-app/src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './TodoTemplate';
+import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>
+      <TodoInsert></TodoInsert>
+      <TodoList todos={todos}></TodoList>
+    </TodoTemplate>
   );
-}
+};
+
+// const App = () => {
+//   return (
+//     <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
