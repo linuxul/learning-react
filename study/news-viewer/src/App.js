8dceaba2759b25c319cf153699a5cb55b6b9b626
linuxul <jsseo@eflash.co.kr>
2024-02-18 14:45:52 +0900

    [feat] 14. 외부 api를 연동하여 뉴스 뷰어 만들기 #14.2

--- study/news-viewer/src/App.js
+++ study/news-viewer/src/App.js
@@ -1,25 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=2cbd90bd1f4c442dbf4cbc42727c4807'
+      );
+      console.log(response);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        ></textarea>
+      )}
     </div>
   );
 }
+
+
+// function App() {
+//   const [data, setData] = useState(null);
+
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(
+//         'https://jsonplaceholder.typicode.com/todos/1'
+//       );
+//       console.log(response);
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         ></textarea>
+//       )}
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const [data, setData] = useState(null);
+
+//   const onClick = () => {
+//     axios
+//       .get('https://jsonplaceholder.typicode.com/todos/1')
+//       .then((response) => {
+//         console.log(response)
+//         setData(response.data);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>블러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         ></textarea>
+//       )}
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
