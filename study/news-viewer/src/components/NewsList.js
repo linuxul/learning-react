cb0ad9f00bd1774303769e4c6e0e7e52257172ed
linuxul <jsseo@eflash.co.kr>
2024-02-18 18:40:27 +0900

    [feat] 14. 외부 api를 연동하여 뉴스 뷰어 만들기 usePromise 사용 #14.5

--- study/news-viewer/src/components/NewsList.js
+++ study/news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,37 +18,26 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  console.log('start NewsList : ', category);
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log('fetchData : ');
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        console.log('query : ' + query)
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2cbd90bd1f4c442dbf4cbc42727c4807`
-        );
-
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2cbd90bd1f4c442dbf4cbc42727c4807`
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
+
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
 
   return (
     <NewsListBlock>
@@ -57,6 +47,48 @@ const NewsList = ({ category }) => {
     </NewsListBlock>
   );
 };
+
+// const NewsList = ({ category }) => {
+//   const [articles, setArticles] = useState(null);
+//   const [loading, setLoading] = useState(false);
+
+//   console.log('start NewsList : ', category);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       console.log('fetchData : ');
+//       setLoading(true);
+//       try {
+//         const query = category === 'all' ? '' : `&category=${category}`;
+//         console.log('query : ' + query)
+//         const response = await axios.get(
+//           `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2cbd90bd1f4c442dbf4cbc42727c4807`
+//         );
+
+//         setArticles(response.data.articles);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [category]);
+
+//   if (loading) {
+//     return <NewsListBlock>대기중...</NewsListBlock>;
+//   }
+
+//   if (!articles) {
+//     return null;
+//   }
+
+//   return (
+//     <NewsListBlock>
+//       {articles.map((article) => (
+//         <NewsItem key={article.url} article={article}></NewsItem>
+//       ))}
+//     </NewsListBlock>
+//   );
+// };
 
 // const sampleArticle = {
 //   title: '제목',
