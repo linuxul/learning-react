db41a958fe4675f03088ad6e062143499d8a473c
linuxul <jsseo@eflash.co.kr>
2024-02-18 18:59:06 +0900

    [feat] 15. Context API #15.1

---
+++ study/context-tutorial/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorBox from './components/ColoBox';
+import ColorContext from './contexts/color';
+
+function App() {
+  return (
+    <ColorContext.Provider value={{ color: 'green'}}>
+      <div>
+        <ColorBox></ColorBox>
+      </div>
+    </ColorContext.Provider>
+  );
+}
+
+// function App() {
+//   return (
+//     <div>
+//       <ColorBox></ColorBox>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
