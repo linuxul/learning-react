89aa3fe82f7d24858f1e2513d1e7116c76c11be3
linuxul <jsseo@eflash.co.kr>
2024-02-19 15:42:48 +0900

    [feat] 19. 코드 스플리팅 : react.lazy 완료 #19.3

--- study/splitting-sample/src/App.js
+++ study/splitting-sample/src/App.js
@@ -1,28 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-// import notify from './notify';
-// import { Component } from 'react';
 import React, { useState, Suspense } from 'react';
-
-const SpliteMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+const SpliteMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SpliteMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SpliteMe></SpliteMe>}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SpliteMe></SpliteMe>}
       </header>
     </div>
   );
 }
+
+// function App() {
+//   const [visible, setVisible] = useState(false);
+//   const onClick = () => {
+//     setVisible(true);
+//   };
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p onClick={onClick}>Hello React!</p>
+//         <Suspense fallback={<div>loading...</div>}>
+//           {visible && <SpliteMe></SpliteMe>}
+//         </Suspense>
+//       </header>
+//     </div>
+//   );
+// }
 
 // class App extends Component {
 //   state = {
