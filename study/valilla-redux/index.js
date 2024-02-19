4f89252ea12bde92450e3ae303fd2df7aff8b838
linuxul <jsseo@eflash.co.kr>
2024-02-19 09:13:22 +0900

    [feat] 17. 리덕스를 사용하여 리액트 앱 상태관리 #17.1

--- study/valilla-redux/index.js
+++ study/valilla-redux/index.js
@@ -1,2 +1,74 @@
-console.log('hello parcel #1')
+import { createStore } from 'redux';
 
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
+
+// state가 undefined일 때는 intialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.defference
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  }
+
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onClick = () => {
+  console.log('toggle')
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onClick = () => {
+  console.log('increate')
+  store.dispatch(increase(1));
+};
+
+btnDecrease.onClick = () => {
+  console.log('decreate')
+  store.dispatch(decrease());
+};
