03a2958b35c25503cfd2ababcd999a66a163648c
linuxul <jsseo@eflash.co.kr>
2024-02-19 14:11:52 +0900

    [feat] 18. 리덕스 미들웨어 비동기 : redux-thunk 처리 #18.5

--- study/learn-redux-middleware/src/containers/SampleContainer.js
+++ study/learn-redux-middleware/src/containers/SampleContainer.js
@@ -13,12 +13,20 @@ const SampleContainer = ({
 }) => {
   // 클래스 형태 컴포넌트였다면 componentDidMount
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect에 파라미터로 넣는 함수는 async 로 할 수 없기 때문에 그 내부에서 async 함수를 선언하고 호출해 줍니ㅏ다.
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
 
-  console.log(post)
-  console.log(users)
+  console.log(post);
+  console.log(users);
 
   return (
     <Sample
@@ -31,11 +39,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS
+    loadingPost: loading['sample/GET_POST'],
+    loadingUsers: loading['sample/GET_UERS']
   }),
   { getPost, getUsers }
 )(SampleContainer);
