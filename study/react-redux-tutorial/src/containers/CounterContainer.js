7ff89e72dc938a020c5f403420ddad72d7ceb3ca
linuxul <jsseo@eflash.co.kr>
2024-02-19 11:12:34 +0900

    [feat] 17. 리덕스를 사용하여 리액트 앱 종료 #17.4

--- study/react-redux-tutorial/src/containers/CounterContainer.js
+++ study/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,27 +1,57 @@
+import { useCallback } from 'react';
 import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
     <Counter
       number={number}
-      onIncrease={increase}
-      onDecrease={decrease}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
     ></Counter>
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter.number
-  }),
-  {
-    increase,
-    decrease
-  }
-)(CounterContainer);
+// const CounterContainer = () => {
+//   const number = useSelector((state) => state.counter.number);
+//   const dispatch = useDispatch();
+//   return (
+//     <Counter
+//       number={number}
+//       onIncrease={() => dispatch(increase())}
+//       onDecrease={() => dispatch(decrease())}
+//     ></Counter>
+//   );
+// };
+
+export default CounterContainer;
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter
+//       number={number}
+//       onIncrease={increase}
+//       onDecrease={decrease}
+//     ></Counter>
+//   );
+// };
+
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number
+//   }),
+//   {
+//     increase,
+//     decrease
+//   }
+// )(CounterContainer);
 
 // export default connect(
 //   (state) => ({
