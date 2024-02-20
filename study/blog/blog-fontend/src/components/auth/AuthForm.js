808df82a3d40e6e83c778b6999915e174fbda76d
linuxul <jsseo@eflash.co.kr>
2024-02-20 10:30:06 +0900

    [feat] 24. 프런트엔드 프로젝트: 시작 및 화원인증 구현 : 회원가입 #24.3

--- study/blog/blog-fontend/src/components/auth/AuthForm.js
+++ study/blog/blog-fontend/src/components/auth/AuthForm.js
@@ -1,10 +1,70 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import theme from '../../lib/styles/theme';
+import Button from '../common/Button';
 
 // 회원가입 또는 로그인 폼을 보여 줍니다.
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+  h3 {
+    margin: 0;
+    color: ${theme.colors.gray[800]};
+    margin-bottom: 1rem;
+  }
+`;
+
+// 스타일된 input
+const StyledInput = styled.input`
+  font-size: 1rem;
+  border: none;
+  border-bottom: 1px solid ${theme.colors.gray[500]};
+  padding-bottom: 0.5rem;
+  outline: none;
+  width: 100%;
+  &:focus {
+    color: ${theme.colors.teal[700]};
+    border-bottom: 1px solid ${theme.colors.gray[700]};
+  }
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+// 폼 하단에 로그인 혹은 회원가입 링크를 보여 줌
+const Footer = styled.div`
+  margin-top: 2rem;
+  text-align: right;
+  a {
+    color: ${theme.colors.gray[600]};
+    text-decoration: underline;
+    &:hover {
+      color: ${theme.colors.gray[900]};
+    }
+  }
+`;
 
 const AuthForm = () => {
-  return <AuthFormBlock>AuthForm</AuthFormBlock>;
+  return (
+    <AuthFormBlock>
+      <h3>로그인</h3>
+      <form>
+        <StyledInput
+          autoComplete="username"
+          name="username"
+          placeholder="아이디"
+        ></StyledInput>
+        <StyledInput
+          autoComplete="new-password"
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+        ></StyledInput>
+        <Button>로그인</Button>
+      </form>
+      <Footer>
+        <Link to="/register">회원가입</Link>
+      </Footer>
+    </AuthFormBlock>
+  );
 };
 
 export default AuthForm;
