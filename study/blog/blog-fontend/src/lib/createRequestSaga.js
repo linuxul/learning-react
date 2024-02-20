51281bcffb5cdcc5cb96830c6a962b17f94b282e
linuxul <jsseo@eflash.co.kr>
2024-02-20 12:10:14 +0900

    [feat] 24. 프런트엔드 프로젝트: 시작 및 화원인증 구현 : 리덕스 모듈 추가 #24.6

---
+++ study/blog/blog-fontend/src/lib/createRequestSaga.js
@@ -0,0 +1,35 @@
+import { call, put } from 'redux-saga/effects';
+import { startLoading, finishLoading } from '../modules/loading';
+
+export const createRequestActionTypes = type => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return [type, SUCCESS, FAILURE]
+}
+
+export default function createRequestSafa(type, request) {
+  // 성공 및 실패 액션 타입을 정의합니다.
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type));
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true
+      });
+    }
+    yield put(finishLoading(type));
+  };
+}
+
+// 사용법: createRequestThunk('GET_USERS', api.getUsers)
--- study/blog/blog-fontend/src/modules/auth.js
+++ study/blog/blog-fontend/src/modules/auth.js
@@ -1,8 +1,14 @@
 import { createAction, handleActions } from 'redux-actions';
 import { produce } from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSaga';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] =
+  createRequestActionTypes('auth/REGISTER');
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] =
+  createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FIELD,
